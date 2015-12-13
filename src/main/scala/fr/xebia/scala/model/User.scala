9861408fdc09396bbfff8fa6a6bf4c8cf17eba37
Fabian Gutierrez <fagossa@gmail.com>
2015-12-13 11:46:42 +0100

    adds additional methods

--- src/main/scala/fr/xebia/scala/model/User.scala
+++ src/main/scala/fr/xebia/scala/model/User.scala
@@ -10,16 +10,88 @@ case class User(
                  gender: Option[String])
 
 object User {
-  def getUserOrElse(firstOption: Option[User], secondOption: Option[User], thirdOption: Option[User]) =
+
+  sealed trait Gender
+
+  object Female extends Gender
+
+  object Male extends Gender
+
+  object NotSpecified extends Gender
+
+  /**
+   * Return the first Option not empty from the list specified using the function
+   * UserOptions#orElse
+   */
+  def getUserOrElse(firstOption: Option[User], secondOption: Option[User], thirdOption: Option[User]): Option[User] =
     orElse(orElse(firstOption, secondOption), thirdOption)
 
   def getUserNameOrElse(someUser: Option[User], defaultName: String) =
     someUser.map(_.firstName).getOrElse(defaultName)
 
+  // Note:
+  // Use Option#filter
+  // valid user means the following criteria:
+  //   - age <= 25
+  //   - called Lawrence
+  //   - with a specified gender
+  def validUser(user: User): Option[User] = {
+    Some(user)
+      .filter(_.age <= 25)
+      .filter(_.lastName == "Lawrence")
+      .filter(_.gender.isDefined)
+  }
+
+  // Note: user pattern matching
+  def translateGender(user: User): Gender = user.gender match {
+    case Some(gender) if gender == "F" => Female
+    case Some(gender) if gender == "M" => Male
+    case None => NotSpecified
+  }
+
+  // Note:
+  // - use UserRepository#findById
+  // - use UserOptions#map
+  def getNaiveGenderFromUserId(id: Int): Option[Option[String]] =
+    map(UserRepository.findById(id))((u) => u.gender)
+
+  // Note:
+  // - use UserRepository#findById
+  // - use UserOptions#flatMap
+  def getBetterGenderFromUserId(id: Int): Option[String] =
+    flatMap(UserRepository.findById(id))((u) => u.gender)
+
 }
 
 object UserOptions {
 
+  /**
+   * apply the mapper function to the option specified and wrap the
+   * result
+   */
+  def map[A, B](maybe: Option[A])(mapper: A => B): Option[B] = {
+    if (maybe.isDefined) {
+      Some(mapper(maybe.get))
+    } else {
+      None
+    }
+  }
+
+  /**
+   * apply the mapper function to the option specified
+   */
+  def flatMap[A, B](maybe: Option[A])(mapper: A => Option[B]): Option[B] = {
+    if (maybe.isDefined) {
+      mapper(maybe.get)
+    } else {
+      None
+    }
+  }
+
+
+  /**
+   * if the first parameter is None return the second
+   */
   def orElse[A](firstOption: Option[A], secondOption: Option[A]): Option[A] =
     if (firstOption.isDefined) {
       firstOption
@@ -30,7 +102,7 @@ object UserOptions {
 }
 
 object UserRepository {
-  private val users = Map(1 -> User(1, "John", "Doe", 32, Some("male")),
+  private val users = Map(1 -> User(1, "John", "Doe", 32, Some("M")),
     2 -> User(2, "Johanna", "Doe", 30, None))
 
   def findById(id: Int): Option[User] = users.get(id)
--- src/test/scala/fr/xebia/scala/model/CollectionExampleSpec.scala
+++ src/test/scala/fr/xebia/scala/model/CollectionExampleSpec.scala
@@ -117,11 +117,39 @@ class CollectionExampleSpec extends FunSpec with Matchers {
 
     it ("should use orElse") {
       val maybeUser1 = Some(User(1, "Akira", "Kurosawa", 50, Some("M")))
-      val maybeUser2 = Some(User(1, "Akira", "Kurosawa", 50, Some("M")))
+      val maybeUser2 = Some(User(2, "Akira", "Toriyama", 55, Some("M")))
       User.getUserOrElse(maybeUser1, None, maybeUser2) shouldBe maybeUser1
       User.getUserOrElse(None, maybeUser2, None) shouldBe maybeUser2
       User.getUserOrElse(None, None, maybeUser2) shouldBe maybeUser2
       User.getUserOrElse(None, None, None) shouldBe None
+    }
+
+    it ("should use filter") {
+      val user1 = User(2, "Christina", "Lawrence", 26, Some("F"))
+      val user2 = User(2, "Angelina", "Jolie", 25, Some("F"))
+      val user3 = User(2, "Jennifer", "Lawrence", 25, Some("F"))
+      val user4 = User(2, "Scarlett", "Lawrence", 25, None)
+      User.validUser(user1) shouldBe None
+      User.validUser(user2) shouldBe None
+      User.validUser(user3) shouldBe Some(user3)
+      User.validUser(user4) shouldBe None
+    }
+
+    it ("should use pattern matching") {
+      val user1 = User(2, "Scarlett", "Johansson", 31, Some("F"))
+      val user2 = User(2, "Michael", "Fassbender", 38, Some("M"))
+      val user3 = User(2, "Titi", "Tata", 25, None)
+      User.translateGender(user1) shouldBe User.Female
+      User.translateGender(user2) shouldBe User.Male
+      User.translateGender(user3) shouldBe User.NotSpecified
+    }
+
+    it ("should use 'Option#map' and 'Option#flatMap'") {
+      // Our first approach could be to simply map the result
+      User.getNaiveGenderFromUserId(1) shouldBe Some(Some("M"))
+      // but as the result is also an Option then we are :(
+      // So, lets try again :D
+      User.getBetterGenderFromUserId(1) shouldBe Some("M")
     }
   }
 }
