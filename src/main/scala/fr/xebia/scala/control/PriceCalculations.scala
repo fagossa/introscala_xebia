7c55e3226d96c047c08886cae649978c8dda31e1
Sergio Dos Santos <sdossantos@xebia.fr>
2016-01-30 17:50:57 +0100

    remove solution for price calculation

--- src/main/scala/fr/xebia/scala/control/PriceCalculations.scala
+++ src/main/scala/fr/xebia/scala/control/PriceCalculations.scala
@@ -8,28 +8,11 @@ object PriceCalculations {
    * TODO PriceCalculations_1: use CollectionTools#zip[A,B](List[A], List[B]):(A,B)
    * Calculate the totalPrice of the list specified ONLY if the film list size IS EQUAL to the prices list
    */
-  def calculateTotalPrice(films: List[Film], qty: List[Int]): Option[Double] =
-    if (films.size == qty.size) {
-      Some {
-        CollectionTools.zip(films, qty)
-          .map { case (film, amt) => film.price * amt }
-          .sum
-      }
-    } else {
-      None
-    }
+  def calculateTotalPrice(films: List[Film], qty: List[Int]): Option[Double] = ???
 
   /*
    * TODO PriceCalculations_2 : use CollectionTools#zipWithIndex[A](List[A]):(A, Int)
    * Return the total price and the index on the ranking ONLY if the film list size IS EQUAL to the prices list
    */
-  def calculateTotalPriceWithIndex(films: List[Film], qty: List[Int]): Option[List[(Int, Double)]] =
-    if (films.size == qty.size) {
-      Some {
-        CollectionTools.zipWithIndex(CollectionTools.zip(films, qty))
-          .map { case ((f: Film, qty: Int), index) => (index, f.price * qty) }
-      }
-    } else {
-      None
-    }
+  def calculateTotalPriceWithIndex(films: List[Film], qty: List[Int]): Option[List[(Int, Double)]] = ???
 }
